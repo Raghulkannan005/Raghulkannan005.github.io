9ab8d727f69462070545dfc5d190051dfcd6e84a
Raghulkannan005 <raghulkannan005@gmail.com>
2024-11-27 19:51:33 +0530

    url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
-  base: '/Raghulkannan005.github.io/',
+  base: '/', //while uploading to git replace '/' with '/Raghulkannan005.github.io/'
   plugins: [react()],
   build: {
     outDir: 'dist',
